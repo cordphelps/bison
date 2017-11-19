47700459b07dcd21968bdff16cbba6f5701ddc62
cord phelps <rc.phelps@gmail.com>
2017-11-19 14:55:26 +0100

    boxplot code

---
+++ boxplot.r
@@ -0,0 +1,5 @@
+# boxplot generation
+
+boxplot(percent.total.aesthetics ~ just.checking, data=bison.df, ylab="news article fraction", xlab="aesthetic keywords", las=1)
+
+boxplot(percent.total.biodiversity ~ just.checking, data=bison.df, ylab="news article fraction", xlab="biodiversity keywords", las=1)
