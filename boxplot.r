f7710d63c0fff97d3f1a7e1427822a9712df86e4
cord phelps <rc.phelps@gmail.com>
2017-11-30 09:13:20 +0100

    r code

--- boxplot.r
+++ boxplot.r
@@ -1,5 +1,9 @@
 # boxplot generation
 
-boxplot(percent.total.aesthetics ~ just.checking, data=bison.df, ylab="news article fraction", xlab="aesthetic keywords", las=1)
+> library(RCurl)
+> source.url <- c("https://raw.githubusercontent.com/cordphelps/bison/master/bison.csv")
+> bison.df <- read.csv(text=getURLContent(source.url), header=TRUE, row.names=NULL)
+> boxplot(percent.total.aesthetics ~ just.checking, data=bison.df, ylab="total: biodiversity + aesthetics", ylim=c(0,1), xlab="aesthetic keywords", las=1, col="powderblue")
+> abline (h=.223, col="blue", lty=4, lwd=4)
+> summary(bison.df$percent.total.aesthetics)
 
-boxplot(percent.total.biodiversity ~ just.checking, data=bison.df, ylab="news article fraction", xlab="biodiversity keywords", las=1)
